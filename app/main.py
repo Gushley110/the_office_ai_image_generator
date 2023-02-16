bb44c9704129737908e0f6a6a9db0d055c4d6c7d
Gushley110 <gushley110@gmail.com>
2023-02-16 03:44:43 -0600

    feat: added test endpoints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.theoffice_api.api_client import TheOfficeAPIClient
+import logging
+from logging.config import dictConfig
+from app.config.log import LogConfig
+from app.config.app import get_settings
+
+dictConfig(LogConfig().dict())
+
+settings = get_settings()
+logger = logging.getLogger(f'{settings.APP_NAME}-log')
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/characters")
+async def get_characters():
+    office_client = TheOfficeAPIClient()
+    characters = await office_client.get_endpoint('characters')
+
+    return characters
+
+
+@app.get("/characters/{character_id}")
+async def get_characters(character_id: str):
+    try:
+        office_client = TheOfficeAPIClient()
+        characters = await office_client.get_endpoint('quotes', character_id)
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+    return characters
 
 
 @app.get("/hello/{name}")
