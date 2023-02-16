9ed6a63dc681575acd3796ac255cbd4b9c4c2fdc
Gushley110 <gushley110@gmail.com>
2023-02-16 04:03:57 -0600

    build: added more tests

--- app/theoffice_api/test_url_builder.py
+++ app/theoffice_api/test_url_builder.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app.theoffice_api.url_builder import URLBuilder
 
 
@@ -11,5 +13,13 @@ def test__build_character_urls():
         item_id_or_random='f_id'
     )
 
+    url_w_random = url_builder.create_url(
+        endpoint='characters',
+        item_id_or_random='random',
+    )
+
     assert url_simple == 'https://www.officeapi.dev/api/characters'
     assert url_w_id == 'https://www.officeapi.dev/api/characters/f_id'
+    assert url_w_random == 'https://www.officeapi.dev/api/characters/random'
+    with pytest.raises(ValueError):
+        url_builder.create_url(endpoint='f_endpoint')
